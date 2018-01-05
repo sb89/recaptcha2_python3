c34edeb0170bae901b1ffab6db79d7ace0278161
Steven Blake <s.blake.08@aberdeen.ac.uk>
2018-01-05 22:26:35 +0000

    Initial commit

---
+++ recaptcha/__init__.py
@@ -0,0 +1,1 @@
+from recaptcha.client import submit
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='recaptcha2_python3',
+      version='0.1',
+      description='Python3 client for Recaptcha v2',
+      url='http://github.com/sb89/recaptcha2_python3',
+      author='Steven Blake',
+      author_email='s.blake.08@aberdeen.ac.uk',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
